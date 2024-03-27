984f27ebd533f91ab3d0991fc5b78e99d4e66a3c
02fe21bcs022 <02fe21bcs022@kletech.ac.in>
2024-03-27 06:18:11 +0530

    Changes to be committed: 	modified:   frontend/flex/src/index.js

--- frontend/flex/src/index.js
+++ frontend/flex/src/index.js
@@ -14,7 +14,6 @@ root.render(
   </React.StrictMode>
 );
 
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitalsdadawd
 
 reportWebVitals();
